955b464be704d7135c059840a0f864e71a23aeb3
m.ibrahim <mohammad.si.tt@gmail.com>
2022-04-11 01:26:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Mohammad",
+    streetAdress: "istanbul, basaksehir"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = { ...employee };
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmp = { ...employee }
+    delete newEmp[key]
+    return newEmp
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
